3f7131faf0764fe010dca37e1662c88e38355d81
Heather Haymond <hheatherjean11@gmail.com>
2024-03-17 15:34:28 -0500

    working on strethch goals. Ready to push and turn in assignment, but still have more goals to stretch.

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -62,14 +62,25 @@ console.log(trueOrFalse(3));  // Expect false, because 3
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 function getLast(array) {
-
+  if(array.length > 0) { // sets conditions 
+    return array.pop(); // takes off last value of array
+  } else {
+    return undefined;
+  }
 }
-
+console.log(getLast([1, 2, 3]));  // Expect 3... the last value
+console.log(getLast([]));         // Expect undefined because the array is empty
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find(value, array) {
-
+ for (let item of array){ // item is declared and scoped within the code block.. it is assigned the current value
+  if (item === value) {
+    return true;
+  }
+ }
+ return false;
+ 
 }
 
 // ----------------------
@@ -78,16 +89,24 @@ function find(value, array) {
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
-}
-
+  if(string.length === 0 ){
+    return false; // indicates that string is empty. no letter is the first letter. 
+  } else if (string[0] === letter){
+    return true; // the 0 aka first fist charecter 
+  } else { // anything is not a letter in the 0 placeholder
+    return false;
+  }
+ }
 
 // 9. Function to return the sum of all numbers in an array
 function sumAll(array) {
   let sum = 0;
   // TODO: loop to add items
-
+  for(let i = 0; i < array.length; i++) {
+    sum+=array[i];
+  }
   // TODO: return the sum
+  return sum;
 }
 
 // 10. Function to return a new array of all positive (greater than zero)
